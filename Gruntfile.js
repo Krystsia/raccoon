122724f32bd372ad18d9fb9eea9fe4abdbf7d0a5
Kiryl Krystsia <Kiryl_Krystsia@epbyminw5776.minsk.epam.com>
2016-02-03 18:34:09 +0300

    third commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,32 @@
 module.exports = function(grunt) {
 
-    // 1. Всё конфигурирование тут
+    
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
+        
+        concat: {
+            dist: {
+                src: [
+                    'js/*.js'
+                ],
+                dest: 'js/production/main.js',
+            }
+        },
         uglify: {
             build: {
-                src: 'js/main.js',
-                dest: 'js/main.min.js'
+                src: 'js/production/main.js',
+                dest: 'js/production/main.min.js'
             }
-        }
+        },
 
     });
 
-    // 3. Здесь мы сообщаем Grunt, что мы планируем использовать этот плагин:
-    grunt.loadNpmTasks('grunt-contrib-concat');
+    
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
 
-    // 4. Мы сообщаем Grunt, что нужно делать, когда мы введём "grunt" в терминале.
-    grunt.registerTask('default', ['uglify']);
+    
+    grunt.registerTask('default', ['concat','uglify']);
 
 };
